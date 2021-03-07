6f6f050828f8c830846f97d816d9df9da5f904b0
MKravtsov <MKravtsov@croc.ru>
2021-03-07 22:04:30 +0300

    Перенос проектов на JS в другой репозиторий

---
+++ FirstASPApp/SecondASPApp/Controllers/TrainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecondASPApp.Controllers
+{
+    public class TrainController : Controller
+    {
+        // GET: Train
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
